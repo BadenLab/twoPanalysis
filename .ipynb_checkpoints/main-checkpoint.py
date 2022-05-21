d0b70c68a1cb911a98aeb64299e059f4915a2b52
Simen Bruøygard <simen.bruoy@gmail.com>
2022-05-21 17:36:10 +0100

    Convenience commit

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -16,6 +16,7 @@ import pathlib
 import utilities
 import quantitative
 import qualitative
+# import quantitative_beta as quantitative
 
 class Experiment:
     def __init__(self, directory,**kwargs):
@@ -35,6 +36,8 @@ class Experiment:
             """            
             
             plane_index = sorted(self.directory.rglob('**/plane*'))
+            if not plane_index:
+                raise ValueError("No Suite2p planes folders identified in directory.")
             return plane_index
         # Function for identifying relevant folders based on plane number
         def index_from_path(plane_path_list):
@@ -105,11 +108,22 @@ class Experiment:
             list_of_names = []
             # Return the immediate content of the directory (i.e. not recursively)
             directory_content = sorted(directory.glob('*'))
-            
+            # Check whether directory is the "final" dir, or a dir of sub-dirs
+            check_dir = []
+            for i in directory_content:
+                check_dir.append(i.is_file())
+            if any(check_dir) is True:
+                _is_dir_of_dirs = False
+                directory_content = directory.parent.glob('*')
+            # else:
+                # _is_dir_of_dirs = True
+            # print(_is_dir_of_dirs)
             # average_signal(obj.fs[1][0].astype(float), obj.trigs[1][0].astype(float), 10)
             
             # Loop through the expeirment folders and grab the relevant data
             print("Retrieving information from:")
+            # if _is_dir_of_dirs is True: # Loop through each folder
+
             for folder in directory_content:
                 print("  -  ", folder)
                 folder_name = folder.name
@@ -117,25 +131,63 @@ class Experiment:
                 tiff_index = sorted(folder.rglob('*.tiff'))
                 # Index and get F trace
                 f_index = sorted(folder.rglob('*/plane*/F.npy'))
-                fs = build_from_index(f_index)
+                # print(f_index)
+                """
+                TODO
+                - Make multi-plane handling possible here...:
+                """
+                fs = build_from_index(f_index)[0]
                 # Identify how mnay cells there are
-                cell_number = fs.shape[1]
+                cell_number = fs.shape[0]
                 ## Index and get trigger trace
                 trig_index = sorted(folder.glob("*.npy"))
                 trigs = build_from_index((trig_index))[0]
                 ## Index and get iscell
                 iscell_index = sorted(folder.rglob('*/plane*/iscell.npy'))
-                # iscell_index = find_files(directory, "iscell", ".npy")
                 iscells = build_from_index(iscell_index)
                 ## Index and get iscell
                 stats_index = sorted(folder.rglob('*/plane*/stat.npy'))
-                # stats_index = find_files(directory, "stat", ".npy")
                 stats = build_from_index(stats_index)
+                ## Index and get spks
+                spks_index = sorted(folder.rglob('*/plane*/spks.npy'))
+                spks = build_from_index(spks_index)
                 ## Index and get ops
                 ops_index = find_files(directory, "ops", ".npy")
                 ops = build_from_index((ops_index))
-                
-                ## For trouble shooting:
+            # if _is_dir_of_dirs is False: # Just grab the folder contents directly
+            #     folder = directory
+            #     print("  -  ", folder)
+            #     folder_name = folder.name
+            #     ## Index .tiff file
+            #     tiff_index = sorted(folder.rglob('*.tiff'))
+            #     # Index and get F trace
+            #     f_index = sorted(folder.rglob('*/plane*/F.npy'))
+            #     # print(f_index)
+            #     """
+            #     TODO
+            #     - Make multi-plane handling possible here...:
+            #     """
+            #     fs = build_from_index(f_index)[0]
+            #     # Identify how mnay cells there are
+            #     cell_number = fs.shape[0]
+            #     ## Index and get trigger trace
+            #     trig_index = sorted(folder.glob("*.npy"))
+            #     trigs = build_from_index((trig_index))[0]
+            #     ## Index and get iscell
+            #     iscell_index = sorted(folder.rglob('*/plane*/iscell.npy'))
+            #     # iscell_index = find_files(directory, "iscell", ".npy")
+            #     iscells = build_from_index(iscell_index)
+            #     ## Index and get stat
+            #     stats_index = sorted(folder.rglob('*/plane*/stat.npy'))
+            #     stats = build_from_index(stats_index)
+            #     ## Index and get spks
+            #     spks_index = sorted(folder.rglob('*/plane*/spks.npy'))
+            #     spks = build_from_index(spks_index)
+            #     ## Index and get ops
+            #     ops_index = find_files(directory, "ops", ".npy")
+            #     ops = build_from_index((ops_index))
+                    
+                # For trouble shooting:
                 # if any((fs.shape[1], iscells.shape[1], stats.shape[1])) != cell_number:
                 #     print(cell_number, "cells. Other shapes:", 
                 #           "fs:", fs.shape,
@@ -145,27 +197,29 @@ class Experiment:
                 #           " Check shapes of files upon import.")
                     # sys.exit()
                 
-                # Throw that data into a dictionary
-                info_dict = {
-                    "folder_name": folder_name,
-                    # "plane_number": number_of_planes,
-                    "f_index": f_index,
-                    "fs": fs,
-                    "cell_numbers" : cell_number,
-                    "tiff_index": tiff_index,
-                    "trig_index": trig_index,
-                    "trigs": trigs,
-                    "iscell_index": iscell_index,
-                    "iscells": iscells,
-                    "stats_index": stats_index,
-                    "stats": stats,
-                    "ops_index": ops_index,
-                    "ops": ops,
-                    "notes" : str()
-                }
-                # Add that dictionary to the list
-                list_of_dicts.append(info_dict)
-                list_of_names.append(folder_name)
+            # Throw that data into a dictionary
+            info_dict = {
+                "folder_name": folder_name,
+                # "plane_number": number_of_planes,
+                "f_index": f_index,
+                "fs": fs,
+                "cell_numbers" : cell_number,
+                "tiff_index": tiff_index,
+                "trig_index": trig_index,
+                "trigs": trigs,
+                "iscell_index": iscell_index,
+                "iscells": iscells,
+                "stats_index": stats_index,
+                "stats": stats,
+                "spks_index": spks_index,
+                "spks": spks,
+                "ops_index": ops_index,
+                "ops": ops,
+                "notes" : str()
+            }
+            # Add that dictionary to the list
+            list_of_dicts.append(info_dict)
+            list_of_names.append(folder_name)
             print(f"List of dictionaries with information from {self.directory} "
                   "created and placed in DataFrame under self.panda.")
             # Make a DataFrame from the list of dictionaries (each row is one experiment)
@@ -178,6 +232,7 @@ class Experiment:
             self.ops = self.panda.loc["ops"]
             self.stats = self.panda.loc["stats"]
             self.iscells = self.panda.loc["iscells"]
+            self.spks = self.panda.loc["spks"]
             self.cell_numbers = self.panda.loc["cell_numbers"]
             self.notes = self.panda.loc["notes"]
             return self.panda 
@@ -205,7 +260,7 @@ class Experiment:
         for i in kwargs:
             self.panda.loc[i] = kwargs[i]
         ## If averages are mentioned, build averages algorithmically    
-        if "averages" in kwargs:
+        if "averages" in kwargs and kwargs["averages"] is True:
             if "mode" not in kwargs:
                 raise ValueError("Missing **kwargs value 'mode', which determines"
                                   "averaging of f-traces.")
@@ -213,28 +268,28 @@ class Experiment:
                 # self.panda.loc["f_avgs"] = None.astype('O')
                 # self.panda.loc["f_trials"] = None.astype('O')
                 # self.panda.loc["trig_trials"] = None.astype('O')
+                ## Initiate cells to overwrite information to
                 self.panda.loc["f_avgs"] = 0
                 self.panda.loc["f_trials"] = 0
                 self.panda.loc["trig_trials"] = 0
-                # f_avg_list = []
-                # f_trial_list = []
-                # trig_trial_list = []
+                self.panda.loc["trig_avgs"] = 0
                 for i, (f, trg, md) in enumerate(
                                     zip(self.panda.loc["fs"], 
                                       self.panda.loc["trigs"], 
                                       self.panda.loc["mode"])):
-                    # print(f, trg, md)
-                    f_avg, f_trial, trig_trial = quantitative.average_signal(f[0], trg, md)
-                    # f_avg_list.append(f_avg)
-                    # f_trial_list.append(f_trial)
-                    # trig_trial_list.append(trig_trial)
-
+                    # if trg.size != 0:
+                    f_avg, f_trial, trig_trial, trig_avg = quantitative.average_signal(f, trg, md)
+                    # else:
+                        # print(f"Warning: experiment number {i} had no trigger channel. Skipping.")
+                        # continue
+                    
                     self.panda.loc["f_avgs"][i] = f_avg
                     self.panda.loc["f_trials"][i] = f_trial
                     self.panda.loc["trig_trials"][i] = trig_trial
+                    self.panda.loc["trig_avgs"][i] = trig_avg
                 
                     # self.panda.loc["trig_trials"] =
-        # f_avg, f_trial, trig_trial = average_signal(obj.fs[1][0], obj.trigs[1], 30)
+        # f_avg, f_trial, trig_trial = quantitative.average_signal(obj.fs[1][0], obj.trigs[1], 30)
         print("Experiment object generated")
     
     
@@ -284,7 +339,7 @@ class Experiment:
         # # Index F file(s)
         # self.f_index = index_from_path(self.plane_paths)
         # self.fs = build_from_index(self.f_index)
-        # # Index .tiff file
+        # # Index .tiff files
         # self.tiff_index = find_files(self.directory, "*_ch1", ".tiff")
         # # Index and get trigger trace 
         # # self.trig_index = find_files(self.directory, "*_ch2", ".npy")
@@ -300,3 +355,23 @@ class Experiment:
         # self.ops_index = find_files(self.directory, "ops", ".npy")
         # self.ops = build_from_index((self.ops_index))
 
+# alt_location = pathlib.Path(r"D:\data_output\TEST_JUPYTER")
+# bad_loc = pathlib.Path(r"D:\data_output\TEST_JUPYTER\nat_mono_2Hz")
+# location = pathlib.Path(r"D:\data_output\TEST_JUPYTER\steps_lowpower_3s_steps")
+# known_working_loc = pathlib.Path(r"D:\data_output\Testing\test_BC_out_AAA")
+# AAAAA_loc = pathlib.Path("D:\data_output\AAAA")
+# laptop = pathlib.Path(r"C:\Users\Simen\OneDrive\Universitet\PhD\test_BC_out\BC_natstim_monoUV_135")
+
+# obj = Experiment(laptop, averages = True, mode = 5)
+# pand = obj.panda
+
+# f_avgs = pand.loc["f_avgs"]
+# f_trials = pand.loc["f_trials"]
+# trig_trials =  pand.loc["trig_trials"]
+# trig_avg = pand.loc["trig_avgs"]
+# # zz = pand.loc["f_avgs"]
+
+# experiment = 0
+# roi = 9
+# # # %matplotlib widget
+# qualitative.plot_averages(pand.loc["f_avgs"][experiment], pand.loc["f_trials"][experiment], pand.loc["trig_trials"][experiment], pand.loc["trig_avgs"][experiment], roi)
