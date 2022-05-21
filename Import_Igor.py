b96f9d18ff9542e988f97c1d13192b3be9878a85
Simen Bruøygard <simen.bruoy@gmail.com>
2022-05-21 17:34:35 +0100

    Trigger trace and image cropping
    Reorganised the way image cropping works, which will now crop images based on their resolutions to crop out blanking artefacts. Calling obj.ch_arrays(resolution) will now overwrite the attribute self.channel1 to be of appropriate dimension, depending on the 'resolution' parameter.

--- Import_Igor.py
+++ Import_Igor.py
@@ -4,50 +4,16 @@ Created on Mon Dec 13 17:54:30 2021
 
 @author: SimenLab
 """
-
-"""
-
-"""
-
-###----- Needed during testing -----
-
-#===== Benchmarking =====
-# import timeit
-# start = timeit.default_timer()
-
-# import cProfile, pstats, io
-# from pstats import SortKey
-# pr = cProfile.Profile()
-# pr.enable()
-#===== Benchmarking =====
-
-
-#==== OS module =====
 import os
-# abspath = os.path.abspath(__file__)
-# dname = os.path.dirname(abspath)
-# print(dname)
-# os.chdir(dname)
-# os.chdir("C:\\Users\\Simen\\OneDrive\\Universitet\\PhD\\Python files\\Modules\\twoPanalysis\\Example data")
-#==== OS module =====
-
- 
-#==== Pathlib =======
 import pathlib
+import pathlib
+import os
+import numpy as np 
+import processing_pypeline.readScanM as rsm
+
 curr_path = pathlib.Path.cwd()
 this_path = pathlib.Path(__file__).resolve().parent
 os.chdir(this_path)
-# print(os.getcwd())
-#==== Pathlib =======
-
-###----- Needed during testing -----
-
-import processing_pypeline.readScanM as rsm
-# import numpy as np
-import pathlib
-import os
-# import tifffile
-import numpy as np 
 
 class get_stack:
     # This class is used in the .smh+.smp to .tiff conversion process, 
@@ -58,10 +24,10 @@ class get_stack:
         self.data_directory = self.file_path.parent
         def change_dir(path_of_file):
             prev_path = pathlib.Path.cwd() #Make note of current (will be previous) working directory
-            print("Making note of old path: {}".format(prev_path))
-            data_path = pathlib.Path(r'{}'.format(path_of_file)) #Construct data path
+            print(f"Making note of old path: {prev_path}")
+            data_path = pathlib.Path(f'{path_of_file}')
             new_path = pathlib.Path(data_path).resolve().parent #Set directory to file location 
-            print("Changing path to: {}".format(new_path))
+            print(f"Changing path to: {new_path}")
             os.chdir(new_path)#Change directory
             print("Done")
             return prev_path
@@ -90,32 +56,91 @@ class get_stack:
         self.channel2 = rsm.to_frame(channel_dict["chan2"], frameTotal=self.frameN, 
                       frameCounter=self.frameC, frameBuffer=self.frameB, 
                       frameHeight=self.frameH, frameWidth=self.frameW)
-        # self.channels_interleved = np.empty((self.channel1.size + self.channel2.size,), dtype=self.channel1.dtype)
-        print("Conversion complete.")
-        """Use: https://numpy.org/doc/stable/reference/generated/numpy.dstack.html"""
-        os.chdir(prev_path) # Go back where we came from
         print("Reverting path.")
+        os.chdir(prev_path) # Go back where we came from
+        print("Conversion complete.")
         
+    def ch_arrays(self, res):
+        ## Due to blanking artefact coming through ScanM, we have to crop the frame 
+        ## down to the resolution we want. Because this artefact may be dependent
+        ## on the selected resolution in ScanM, we will make a quick LUT to map 
+        ## resolution down to crop. Fill in as needed...
+        resolution_map = { # Read tuple as left_crop, right_crop
+            "128": (34, 32),
+            "256": (28, 28),
+            "512": (56, 56),
+            "likely_fine": (35, 30),
+            "no_crop": (),
+            }
+        # NOTE: Cropping note needed on y-axis   
+        if res == 128:
+            self.channel1 = self.channel1[:, :, resolution_map["128"][0]:res+resolution_map["128"][1]]
+        if res == 256:
+            self.channel1 = self.channel1[:, :, resolution_map["256"][0]:res+resolution_map["256"][1]]
+        if res == 512:
+            self.channel1 = self.channel1[:, :, resolution_map["512"][0]:res+resolution_map["512"][1]]        # "Safe" setting, even if output res will be a bit lower than ideal
+        if res == "likely_fine":
+            self.channel1 = self.channel1[:, :, resolution_map["likely_fine"][0]:res+resolution_map["likely_fine"][1]]        # "Safe" setting, even if output res will be a bit lower than ideal
+        return self.channel1, self.channel2
 
-def get_ch_arrays(stack_obj, res):
-    # return exp_obj.channel1[:, 0:256, 35:256+35], exp_obj.channel2[:, 0:256, 35:256+35] 
-    return stack_obj.channel1[:, 0:res, 35:res+35], stack_obj.channel2[:, 0:res, 35:res+35] 
+    def header_dict(stack_obj):
+        header_dict = {
+            'NumberOfFrames': stack_obj.frameN,
+            'FrameCounter'  : stack_obj.frameC,
+            'StimBufPerFr'  : stack_obj.frameB,
+            'FrameHeight'   : stack_obj.frameH,
+            'FrameWidth'    : stack_obj.frameW,
+            }
+        return header_dict
+    def trigger_trace_frame(self):
+        #Binarise the trigger using frame-wise percision (fast but impercise)
+        ## Make an array of appropriate dimension
+        trigger_trace_arr = np.zeros((1, self.channel2.shape[0]))[0]
+        ## Loop through trigger image array
+        for frame in range(self.channel2.shape[0]):
+            if np.any(self.channel2[frame] > 1):
+                trigger_trace_arr[frame] = 1
+            #If trigger is in two consecutive frames, just use the first one so counting is correct
+            if trigger_trace_arr[frame] == 1 and trigger_trace_arr[frame-1] == 1: 
+                trigger_trace_arr[frame] = 0
+        return trigger_trace_arr
+    def trigger_trace_line(self):
+        #Binarise the trigger using line-wise percision (slow but guaranteed percision)
+        ## Make empty array that has dims frame_number x frame_size (for serialising each frame)
+        trigger_trace_arr = np.empty((len(self.channel2), self.channel2[0].size))
+        ## Loop through the input trigger array and serialise each frame
+        for n, frame in enumerate(self.channel2):
+            serial = frame.reshape(1, frame.size)
+            ## Place that serialised data in its correct index
+            trigger_trace_arr[n] = serial
+        ## Our matrix is now an array of vectors containing serialised information from each frame
+        ## Reshape this matrix into one long array (pixel x pixel-value)
+        serial_trigger_trace = trigger_trace_arr.reshape(1, self.channel2.size)
+        ## Then we binarise the serialised trigger data
+        binarised_trigger_trace = np.where(serial_trigger_trace > 10000, 1, 0)[0]
+        ## Boolean search for whether index n > n-1 (basically rising flank detection) 
+        trig_onset_serial = binarised_trigger_trace[:-1] > binarised_trigger_trace[1:]
+        ## Get the frame indeces for trigger onset
+        trig_onset_index = np.where(trig_onset_serial > 0)
+        ## Then divide each number in trig_onset_index by the amount of lines
+        trigg_arr_shape = self.channel2.shape
+        lines_in_scan = trigg_arr_shape[1] * trigg_arr_shape[2]
+        frame_of_trig = np.around(trig_onset_index[0]/lines_in_scan, 0)
+        ## Convert back to frames 
+        frame_number = len(self.channel2)
+        trig_trace = np.zeros(frame_number)
+        for i in frame_of_trig:
+            trig_trace[int(i)] = 1
+        return trig_trace
 
-def get_header_dict(stack_obj):
-    header_dict = {
-        'NumberOfFrames': stack_obj.frameN,
-        'FrameCounter'  : stack_obj.frameC,
-        'StimBufPerFr'  : stack_obj.frameB,
-        'FrameHeight'   : stack_obj.frameH,
-        'FrameWidth'    : stack_obj.frameW,
-        }
-    return header_dict
-    
+
+
 # Make trigger_trace --> This should be addedd to the get_stack class? 
 def trigger_trace(trigger_arr):
-    trigger_trace_arr = np.zeros((1, trigger_arr.shape[0]))[0]
-    for frame in range(trigger_arr.shape[0]):
-        if np.any(trigger_arr[frame] > 1):
+    raise DeprecationWarning("Calling trigger trace from Import_Igor is depricated. Instead, please call either trigger_trace_frame or trigger_trace_line from Import_Igor.get_stack() object.. This may change in the future.")
+    trigger_trace_arr = np.zeros((1, self.channel2.shape[0]))[0]
+    for frame in range(self.channel2.shape[0]):
+        if np.any(self.channel2[frame] > 1):
             trigger_trace_arr[frame] = 1
         #If trigger is in two consecutive frames, just use the first one so counting is correct
         if trigger_trace_arr[frame] == 1 and trigger_trace_arr[frame-1] == 1: 
@@ -123,46 +148,3 @@ def trigger_trace(trigger_arr):
         # else:
             # trigger_trace_arr[frame] = 0
     return trigger_trace_arr
-
-"""
-______________________________Testing__________________________________________
-"""
-
-# test = make_stack(-5000, 10000, ((256, 256, 800)))
-# input_file = r"C:\Users\SimenLab\OneDrive\Universitet\PhD\Python files\Git repos\2Panalysis\1_8dfp_ntc3_512_1.smp"
-# a = get_stack(input_file)
-# # img = get_stack(r"C:\Users\SimenLab\OneDrive\Universitet\PhD\Python files\Git repos\2Panalysis", "1_8dfp_ntc3_512_1")
-# a = get_stack(input_file)
-
-# img_for_tif = get_ch_arrays(img, 512)
-# tifffile.imsave(r"C:\Users\SimenLab\OneDrive\Universitet\PhD\Python files\Suite2p tests\test.tiff", img_for_tif[0])
-
-# img1 = get_stack("L2 expanding circles 3-700")
-# img1 = get_stack("L5-20my l10x10 100um")
-
-# def stack_average(stack):
-#     for i in len(stack.shape[x]):
-#         Get every pixel in X axis 
-#         for i in len(stack.shape[y]):
-#             Get every pixel in Y axis 
-#             a = 1
-#             return 
-# img1.channel1[:, 1, 2] <-- The time axis
-
-
-# img1 = get_stack("L6 grid whole screen 600")
-
-
-
-#_____________________________
-# stop = timeit.default_timer()
-# print('Time (sec.): ', stop - start)
-# print('Time (min.): ', (stop - start)/60)
-
-#_____________________________
-# pr.disable()
-# s = io.StringIO()
-# sortby = SortKey.CUMULATIVE
-# ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-# ps.print_stats()
-# print(s.getvalue())
