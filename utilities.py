be522b2f95ca4beca4c306465f47e566ae0e1020
Simen Bruøygard <simen.bruoy@gmail.com>
2022-05-25 21:07:34 +0100

    Pickles and line-precision
    Added ability to pickle experiment objects. Plus, added generating trigger trace by reading out single lines in trigger channel, rather than by frame. Should greatly improve precision of trigger trace.

--- utilities.py
+++ utilities.py
@@ -10,8 +10,9 @@ import numpy as np
 import tifffile
 import warnings
 import shutil
-import Import_Igor
+import pickle
 import os
+import Import_Igor
 
 class file_handling:
     def load_experiment(f_path, trigger_path):
@@ -86,10 +87,11 @@ class file_handling:
         path_of_tiffs = sorted(directory.glob('*.tiff'))
         path_of_trigs = sorted(directory.glob('*.npy'))
         if len(path_of_trigs) == 0 and len(path_of_tiffs) == 0:
+            print("No files found. Exiting.")
             return
         if len(path_of_trigs) == 0:
             print(path_of_trigs)
-            warnings.warn("No trigger channel detected. No .npy file generated.")
+            warnings.warn("No .npy file detected. No trigger channel generated.")
             for tiff in path_of_tiffs:
                 ### Step 3.1: Make folder with tiff filename
                 new_single_plane_folder = directory.joinpath(
@@ -120,22 +122,13 @@ class file_handling:
                     trig, new_single_plane_folder)).with_suffix(".npy")
                 current_trig_name = trig_new_location.stem
                 trig_paths.append(current_trig_name)
-                ### Optional: Rename files to add channel number
-                # current_tiff_final_location =  tiff_new_location.rename(
-                #     tiff_new_location.with_stem(
-                #     f"{current_tiff_name}_ch1").with_suffix(".tiff"))
-                # tiff_paths.append(current_tiff_final_location)
-                # current_tiff_final_location =  tiff_new_location.rename(
-                #     tiff_new_location.with_stem(
-                #     f"{current_trig_name}_ch2").with_suffix(".npy"))
-                # tiff_paths.append(current_tiff_final_location)
         path_of_tiffs = sorted(directory.rglob('*.tiff'))
         path_of_trigs = sorted(directory.rglob('*.npy'))
         return path_of_tiffs, path_of_trigs
     
 class data:
     ## Algorithmically generate .tiffs and .npy (image and trigger) from Igor 
-    def gen_tiffs_from_igor(input_folder, output_folder, crop):
+    def gen_tiffs_from_igor(input_folder, output_folder, crop, **kwargs):
         input_folder = pathlib.Path(input_folder)
         output_folder = pathlib.Path(output_folder)
         img_count = 0
@@ -145,7 +138,9 @@ class data:
         pre_existing_content_names = []
         for i in pre_existing_content:
             pre_existing_content_names.append(i.stem)
-        for file in input_folder.iterdir():
+        for file in input_folder.glob('*.smp'):
+            if file.with_suffix(".smh").exists() is False:
+                raise FileNotFoundError(f"Could not find accompanying header (.smh) file for {file} in {input_folder}.")
             img_count += 1
             if file.stem in pre_existing_content_names:
                 warnings.warn("Input files and output files have the same name, skipping conversion. Please manually delete files in output folder to force conversion.")
@@ -154,15 +149,17 @@ class data:
                 file = pathlib.Path(file).resolve()
                 img = Import_Igor.get_stack(file)
                 img_name = file.stem
-                
-                """Current target"""
-                # This is not really a good method...:
-                img_arr, trigger_arr = img.get_ch_arrays(img, crop)
-                
-                
-                
-                """This WILL be depricated: This is where you need to make a kwarg..."""
-                trigger_trace = Import_Igor.trigger_trace(trigger_arr) # Algorithmically get the trigger trace out of trigger channel
+                img_arr, trigger_arr = img.ch_arrays(crop)
+                ## Select precision of trigger trace to 
+                ## algorithmically get the trigger trace out of trigger channel
+                if "trigger_precision" in kwargs:
+                    if kwargs["trigger_precision"] == "line":
+                        trigger_trace = img.trigger_trace_line()
+                    if kwargs["trigger_precision"] == "frame":
+                        trigger_trace = img.trigger_trace_frame()
+                # Default to line-wise precision
+                else:
+                    trigger_trace = img.trigger_trace_line()
                 # save_folder = pathlib.Path(r".\Data\data_output\{}".format(img_name)) # Bit more elegant than above
                 tiff_path = output_folder.joinpath(
                     img_name).with_suffix(".tiff")
@@ -173,9 +170,16 @@ class data:
                 except FileNotFoundError:
                     os.mkdir(tiff_path.parent)
                 np.save(trig_path, trigger_trace)
+                if "pickle" in kwargs and kwargs["pickle"] is True:
+                    img_obj_path = output_folder.joinpath(
+                        "TEMP_pickle").with_suffix(".pickle")
+                    with open(img_obj_path, 'wb') as f:
+                        pickle.dump(img, f)
                 del img, img_name, img_arr, trigger_arr, file
             if img_count == 0:
                 raise TypeError("No Igor .smh or .smp files were identified!")
+
+
     def crop(f, trigger, start_buffer, end_buffer):
         """
         Crop the 
@@ -271,7 +275,7 @@ class data:
     # Make trigger_trace
     def trigger_trace_by_frame(trigger_arr):
         raise DeprecationWarning("Calling trigger trace from Import_Igor is depricated. Instead, please call attribute trigger_trace_frame from Import_Igor.get_stack obj instead. This may change in the future.")
-        #Binarise the trigger using frame-wise percision (fast but impercise)
+        #Binarise the trigger channel using frame-wise percision (fast but impercise)
         ## Make an array of appropriate dimension
         trigger_trace_arr = np.zeros((1, trigger_arr.shape[0]))[0]
         ## Loop through trigger image array
@@ -284,7 +288,7 @@ class data:
         return trigger_trace_arr
     def trigger_trace_by_line(trigger_arr):
         raise DeprecationWarning("Calling trigger trace from Import_Igor is depricated. Instead, please call attribute trigger_trace_line from Import_Igor.get_stack obj instead. This may change in the future.")
-        #Binarise the trigger using line-wise percision (slow but guaranteed percision)
+        #Binarise the trigger channel using line-wise percision (slow but guaranteed percision)
         ## Make empty array that has dims frame_number x frame_size (for serialising each frame)
         trigger_trace_arr = np.empty((len(trigger_arr), trigger_arr[0].size))
         ## Loop through the input trigger array and serialise each frame
